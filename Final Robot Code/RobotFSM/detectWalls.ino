3ab2dd45e9442873b838637c12790e713770f28e
franceskoback <31418742+franceskoback@users.noreply.github.com>
2017-10-30 14:41:22 -0400

    Update detectWalls.ino

--- Final Robot Code/RobotFSM/detectWalls.ino
+++ Final Robot Code/RobotFSM/detectWalls.ino
@@ -6,9 +6,18 @@
  
 byte detectWalls() {
   byte wallData = 0;
-  
+  int wallArray[3];
+ 
   for (int i = 0; i < 3; i ++) {    
     wallArray[i] = isWall(wallPinArray[i]);
+  }
+ 
+  if (wallArray[2]==1){
+    bitSet(wallData,0);
+  } if (wallArray[1]==1){
+    bitSet(wallData,1);
+  } if (wallArray[0]==1){
+    bitSet(wallData,2);
   }
 
   return wallData;
