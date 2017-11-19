4bace298d88814961702fccc4fd2227e217d4f0a
katherinejlu <kl645@cornell.edu>
2017-11-18 21:10:59 -0500

    Update inputreader3.v

--- code dump/Final FPGA Code/inputreader3.v
+++ code dump/Final FPGA Code/inputreader3.v
@@ -4,9 +4,10 @@ arduinoInput,
 robotX,
 robotY,
 preX,
-preY),
+preY,
 walls,
-treasure;
+treasure, 
+left, right, down, up);
 
 input valid;
 input [7:0] arduinoInput;
@@ -14,6 +15,10 @@ output reg [1:0] robotX;
 output reg [2:0] robotY;
 output reg [3:0] walls;
 output reg [1:0] treasure;
+output reg left; 
+output reg right; 
+output reg up; 
+output reg down; 
 
 output reg [1:0] preX;
 output reg [2:0] preY;
@@ -21,14 +26,44 @@ output reg [2:0] preY;
 always @ (posedge valid) begin
 	preX = robotX;
 	preY = robotY;
-	if (arduinoInput[8] == 1) begin
+
+	if (arduinoInput[8] == 1'b1) begin
 		robotX = arduinoInput[6:5];
 		robotY = arduinoInput[4:2];
 	end
-	if arduinoInput[8] == 0) begin
+	if (arduinoInput[8] == 1'b0) begin
 		walls = arduinoInput[6:3];
 		treasure = arduinoInput[2:1];
 	end
+	
+	if (preX == robotX) begin 
+		if (robotY > preY) begin 
+			right = 1'b0; 
+			left = 1'b0;
+			up = 1'b1;
+			down = 1'b0; 
+		end else begin 
+			right = 1'b0; 
+			left = 1'b0; 
+			up = 1'b0;
+			down = 1'b1;
+		end
+	end 
+
+	if (preY == robotY) begin 
+		if (robotX > preX) begin 
+			right = 1'b1; 
+			left = 1'b0;
+			up = 1'b0;
+			down = 1'b0; 
+		end else begin 
+			right = 1'b0; 
+			left = 1'b1; 
+			up = 1'b0;
+			down = 1'b0;
+		end
+	end 
+
 end
 
 endmodule
