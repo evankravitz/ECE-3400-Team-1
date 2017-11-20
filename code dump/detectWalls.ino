4f66e845bf3c6fe434c1fce56c053eae2f63b917
rchinni <rpc222@cornell.edu>
2017-11-20 12:01:56 -0500

    OLD DFS

---
+++ code dump/detectWalls.ino
@@ -0,0 +1,36 @@
+void detectWalls() {
+
+  //Serial.println("Detect walls");
+   wallLeft = false;
+   wallRight = false;
+   wallMid = false;
+
+//  for(int i=0; i<3; i++){
+     distanceLeft = analogRead(wallPinLeft);
+     distanceMid = analogRead(wallPinMid);
+     distanceRight = analogRead(wallPinRight);
+//  }
+ 
+//  Serial.print("left:");
+//  Serial.println(distanceLeft);
+  if (distanceLeft > 150) {
+    wallLeft = true;
+  }
+//  Serial.print("middle:");
+//  Serial.println(distanceMid);
+  if (distanceMid > 150) {
+    wallMid = true;
+  }
+//  Serial.print("right:");
+//  Serial.println(distanceRight);
+  if (distanceRight > 150) {
+    wallRight = true;
+  }
+  
+//  Serial.print("left:");
+//  Serial.println(wallLeft);
+//  Serial.print("middle:");
+//  Serial.println(wallMid);
+//  Serial.print("right:");
+//  Serial.println(wallRight);
+}
